fc2f2adf09447a987b1911898df55b99807299a8
Trollface8D <82329632+Trollface8D@users.noreply.github.com>
2024-11-13 10:33:31 +0000

    Added testcase and fixed the bugs

--- calculator.py
+++ calculator.py
@@ -3,23 +3,34 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        for i in range(0,abs(b)):
             result = self.add(result, a)
+        if(b < 0):
+            result *= -1;
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
+        if(a>=0):
+            while abs(a) >= abs(b):
+                a = self.subtract(a, abs(b))
+                result += 1
+            if(b<0):
+                result *=-1
+        else:
+            while abs(a) >= abs(b):
+                a = self.add(a, abs(b))
+                result += 1
+            if(b>=0):
+                result *= -1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,19 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1, 2), 1)
+
+        self.assertEqual(self.calc.subtract(10,4),6)
+        self.assertEqual(self.calc.subtract(-3,2),-5)
+
+        self.assertEqual(self.calc.multiply(3,2),6)
+        self.assertEqual(self.calc.multiply(-5,-10),50)
+
+        self.assertEqual(self.calc.divide(6,2),3)
+        self.assertEqual(self.calc.divide(-10,2),-5)
+
+        self.assertEqual(self.calc.modulo(5,2),1)
+        self.assertEqual(self.calc.modulo(100,10),0)
 
     # Add the following test methods to the TestCalculator class:
 
